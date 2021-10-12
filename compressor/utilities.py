4fecae5b2f97bac94c0bde513a1b1d5416da2eec
satshree <satshree.shrestha@yahoo.com>
2021-10-12 11:50:50 -0500

    update compression algorithm

--- compressor/utilities.py
+++ compressor/utilities.py
@@ -1,10 +1,17 @@
 from PIL import Image
 from io import BytesIO
 from django.core.files.base import ContentFile
+from converter.utilities import convert_image
 
 
 def compress_image(image_to_compress, limit):
     image = Image.open(image_to_compress)
+    img_format = image.format.lower()
+
+    if img_format == "png":
+        image = Image.open(convert_image(
+            image_to_convert=image_to_compress))
+
     compressed_image_io = BytesIO()
 
     image.save(compressed_image_io, format=image.format, quality=limit)
@@ -13,5 +20,8 @@ def compress_image(image_to_compress, limit):
         compressed_image_io.getvalue(), compressed_file_name)
 
     image.close()
+
+    if img_format == "png":
+        compressed_file = convert_image(image_to_convert=compressed_file)
 
     return compressed_file
