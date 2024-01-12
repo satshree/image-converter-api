66b06142d6a8aa321102783222f2aa4921c37680
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2024-01-12 13:41:02 -0600

    updated build file

--- build.sh
+++ build.sh
@@ -18,4 +18,4 @@ python -m pip install -r requirements.txt
 
 # Collect static files
 echo "Collecting static files..."
-python manage.py collectstatic --noinput --clear
+python manage.py collectstatic --noinput -v 3
